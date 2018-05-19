05c6de9d0afbe4bc2846004077ddda64baa9dc64
William LeGate <william@legate.me>
2018-05-19 14:26:05 -0700

    Updates player.js

--- server/model/game.js
+++ server/model/game.js
@@ -3,19 +3,21 @@ const db = require("./../database");
 const createGame = (req, res, next) => {
   // TODO: save created game as the value of the res.locals.game key
   // req.body will have the key quizID which should be saved to the game model
-  // const query = `INSERT INTO player(displayName) VALUES($1)`;
-  // const values = ["SomeDisplayName"];
-  // // callback
-  // db.query(query, (err, res) => {
-  //   console.log("DB response");
-  //   if (err) {
-  //     console.log(err.stack);
-  //   } else {
-  //     console.log(res);
-  //     // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-  //   }
-  //   next();
-  // });
+  const query = `INSERT INTO player(displayName) VALUES($1) RETURNING *`;
+  const values = ["SomeDisplayName"];
+
+  // callback
+  db.query(query, values, (err, res) => {
+    console.log(`DB response:`);
+    console.dir(res.rows[0]);
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log(res);
+      // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    }
+    next();
+  });
 };
 
 const getGame = (req, res, next) => {};
--- server/model/player.js
+++ server/model/player.js
@@ -1,7 +1,28 @@
-const createPlayer = (displayName) => {
-  
-}
+const db = require("./../database");
 
-const createPlayer = (displayName) => {
+const createPlayer = (req, res, next) => {
+  // TODO: save created game as the value of the res.locals.game key
+  // req.body will have the key quizID which should be saved to the game model
+  const query = `INSERT INTO player(displayName) VALUES($1) RETURNING *`;
 
-}
+  // TODO: change with correct value
+  const values = ["SomeDisplayName"];
+
+  // callback
+  db.query(query, values, (err, res) => {
+    if (err) {
+      // handle error
+    } else {
+      const player = res.rows[0];
+      // do something
+      next();
+    }
+  });
+};
+
+const getPlayer = (req, res, next) => {};
+
+module.exports = {
+  createPlayer,
+  getPlayer
+};
