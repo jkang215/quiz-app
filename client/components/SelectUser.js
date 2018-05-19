5b25be97376b4eee11c110f834586b3ecb59decd
William LeGate <william@legate.me>
2018-05-19 12:24:51 -0700

    Merges pull request into local repository

--- client/components/SelectUser.js
+++ client/components/SelectUser.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const SelectUser = (props) => {
-
+const SelectUser = props => {
   return (
-      <form id="selectUser-form">
-        <div className="form-group">
-          <label htmlFor="select-field">Select User</label>
-          <input id="select-field" className="form-control" name="username" type="text" placeholder="Username" />
-          <button className="btn btn-primary" onClick={() => props.showLobby()}>Submit User</button>
-        </div>
-      </form>
+    <form id="selectUser-form">
+      <div className="form-group">
+        <label htmlFor="select-field">Select User</label>
+        <input
+          id="select-field"
+          className="form-control"
+          name="username"
+          type="text"
+          placeholder="Username"
+        />
+        <button className="btn btn-primary" onClick={() => props.showLobby()}>
+          Submit Usery
+        </button>
+      </div>
+    </form>
   );
 };
 
